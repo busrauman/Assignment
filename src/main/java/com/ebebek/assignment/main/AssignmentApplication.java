d0b12d1d181e545f8133dc63db8717606817a9d2
busrauman <busrauman93@gmail.com>
2018-07-20 00:01:50 +0300

    initialize spring boot app

---
+++ src/main/java/com/ebebek/assignment/main/AssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.ebebek.assignment.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentApplication.class, args);
+	}
+}
